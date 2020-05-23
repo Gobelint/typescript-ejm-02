f7c596befcd509a0adc5924b51026488ecab1467
Watlook <jamal210free@hotmail.com>
2020-05-23 16:53:39 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-ejm-02
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-ejm-02)
---
+++ index.ts
@@ -0,0 +1,12 @@
+let radio:number = 4;
+let altura:number = 15;
+let volumen:number = 0;
+let area:number = 0;
+
+volumen = Math.PI * (radio ** 2) * altura;
+area = 2 * Math.PI * radio * altura;
+
+console.log(`Radio : ${radio}`);
+console.log(`Altura : ${altura}`);
+console.log(`Volumen : ${volumen}`);
+console.log(`Altura : ${altura}`);
